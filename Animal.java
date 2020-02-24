7be23738cbed592a246423587cc4f8c0e4fda1c9
Kevin Koudelka <kevinkoudelka@wifi-roaming-128-4-121-52.host.udel.edu>
2020-02-24 18:32:36 -0500

    Fixed .java

---
+++ Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+	public int Legs;
+	
+	Animal(int x) {
+		this.Legs = x;
+	}
+	public int getLegs() {
+		return Legs;
+}
+}
+
+
---
+++ Dog.java
@@ -0,0 +1,14 @@
+import Animal.class
+
+public class Dog extends Animal {
+	String name;
+	int Legs;
+	
+	Dog(string n, int l) {
+		this.name = n;
+		this.Legs = l;
+	}
+	public int getLegs() {
+		return Legs;
+}
+}
